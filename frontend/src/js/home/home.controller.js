749a7d6dbeccfcf6ee1fb0b6f470404762c1327a
Dani Ortiz <daniortizgar@gmail.com>
2018-10-03 11:07:31 +0200

    Home finalizado(slider,imagen sectors)

--- frontend/src/js/home/home.controller.js
+++ frontend/src/js/home/home.controller.js
@@ -1,10 +1,14 @@
 class HomeCtrl {
   constructor(AppConstants, $scope, sectors) {
     'ngInject';
- 
-    console.log(sectors);
+    
+    this.myInterval = 5000;
+    this.noWrapSlides = false;
     this.appName = AppConstants.appName;
     this._$scope = $scope;
+    this.slides = [ {image:'/images/portada1.jpg',text:"Get funding for your code projects.",id:0},
+                    {image:'/images/portada2.jpg',text:"Supports other projects to keep the community growing.",id:1},
+                    {image:'/images/portada3.jpg',text:"Other users can do the code you don't know how to do.",id:2}];
 
     if (sectors){
       $scope.infoSect = sectors.slice(0, 3);
